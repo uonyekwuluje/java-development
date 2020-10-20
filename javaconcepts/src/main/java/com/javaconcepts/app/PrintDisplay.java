d8491e0def98b638c09af93b8cc493bd7edfbc4e
uonyekwuluje <uionyekwuluje@gmail.com>
2020-10-20 04:33:45 -0400

    updating java code

--- javaconcepts/src/main/java/com/javaconcepts/app/PrintDisplay.java
+++ javaconcepts/src/main/java/com/javaconcepts/app/PrintDisplay.java
@@ -6,8 +6,8 @@ public class PrintDisplay
     {
         int num1 = 23;
         int num2 = 12;
-        System.out.println("Welcome to my second class");
+        System.out.println("Welcome to my second class. Addition");
         System.out.println("The Sum of "+num1+" and "+num2+" = "+(num1 + num2));
-        System.out.println("\n\n\n");
+        System.out.println("\n\n");
     }
 }
