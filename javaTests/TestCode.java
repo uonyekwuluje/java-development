56543dc6afcf04e437f514c4e9b0ee2e46f62175
uonyekwuluje <uionyekwuluje@gmail.com>
2020-10-22 22:07:23 -0400

    updating practice code

--- javaTests/TestCode.java
+++ javaTests/TestCode.java
@@ -2,14 +2,36 @@ import java.util.Scanner;
 
 public class TestCode {
     public static void main(String[] args){
-        final int RADUS = 23; 
+        double dnum = 34.90; 
+        boolean tval = true;
+
+
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter a Number: ");
+        /*System.out.print("Enter a Number: ");
         int num1 = input.nextInt();
+      
+        if ((int)dnum >= 50){ 
+            System.out.println("Great Stuff");
+        }else{
+            System.out.println("Great Bad Stuff");
+        }
+        System.out.println("2 to the power 2 = "+Math.pow(2,2));
+      
+        if (tval)
+          System.out.println("Well It is True");
+        else
+          System.out.println("Terribly Bad");*/
+       
+       String str = new String();
+       System.out.print("Enter Full Name: ");
+       str = input.nextLine();
+       System.out.println("Normal :"+str);
+       System.out.println("Lower :"+str.toLowerCase());
+       System.out.println("Lower :"+str.toUpperCase());
+       System.out.println("Lower :"+str.length());
 
-        System.out.print("Enter a Number: ");
-        int num2 = input.nextInt();
+       String message = "Welcome to Grafton Massachusetts";
+       System.out.println(message.substring(0,11));
 
-        System.out.println("Sum of "+num1+" and "+num2+" = "+(num1 + num2)); 
     }
 } 
---
+++ javaTests/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+javac TestCode.java
+java TestCode
