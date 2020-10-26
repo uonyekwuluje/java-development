6f7643d7aa9b0518dc9182d6f412867c2e78c4c3
uonyekwuluje <uionyekwuluje@gmail.com>
2020-10-26 03:16:57 -0400

    updating test code

--- javaTests/JaPlay.java
+++ javaTests/JaPlay.java
@@ -5,15 +5,12 @@ public class JaPlay {
       Scanner tinput = new Scanner(System.in);
 
       String fullName = "Uchechukwu Onyekwuluje";
-      char[] fName = new char[fullName.length()];
-
-      System.out.println(fullName); 
-      System.out.println(fullName.length()); 
-      for (int x = 0; x<=fullName.length()-1; x++) 
-          fName[x] = fullName.charAt(x);
-
-      for (char e: fName){
-         System.out.println(e);
+      int[][] newt = {{2,3},{6,1},{8,4}};
+      for (int row = 0; row <= newt.length - 1; row++){
+         for (int col = 0; col <= newt[0].length -1; col++){
+              System.out.print(newt[row][col]);
+          }
+          System.out.println("");
       }
    }
 }
--- javaTests/run.sh
+++ javaTests/run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 javac JaPlay.java
 java JaPlay
+rm JaPlay.class
