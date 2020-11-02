82ed31ab54579baaadd77b74e6abe0237eefcb80
uonyekwuluje <uionyekwuluje@gmail.com>
2020-11-02 00:52:35 -0500

    update on string test

---
+++ javaTests/strings/TestStrings.java
@@ -0,0 +1,30 @@
+public class TestStrings{
+   public static void main(String[] args){
+       char[] arrSample = {'T','O','N','y'};
+       String nstring = new String(arrSample);
+       String str1 = "Natasha";
+       String str2 = "Romanov";
+       System.out.println(arrSample[0]);
+       System.out.println(nstring);
+       concatstring(str1,str2);
+       indexstring(str1,str2);
+   }
+
+   public static void concatstring(String ar1, String ar2){
+       System.out.println("Testing String Concatenation\n----------------------------");
+       String ar3 = ar1 + ar2;
+       String ar4 = ar1.concat(ar2);
+       System.out.println("Full name => "+ar3);
+       System.out.println("Lenght of String => "+ar4.length());
+   }
+
+
+   public static void indexstring(String ar1, String ar2){
+       System.out.println("\nTesting String Index\n----------------------------");
+       String ar3 = ar1 + ar2;
+       String ar4 = ar1.concat(ar2);
+       System.out.println("Full name => "+ar3.charAt(5));
+       System.out.println("Lenght of String => "+ar4.indexOf('R'));
+   }
+
+}
