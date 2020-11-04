3b9da4baa7a6442296aabdbcfa395e58d918423a
uonyekwuluje <uionyekwuluje@gmail.com>
2020-11-04 01:27:33 -0500

    initial commit

---
+++ javaExercises/reverse_integer/ReverseInteger.java
@@ -0,0 +1,11 @@
+public class ReverseInteger{
+    public static void main(String[] args){
+       System.out.println("Main Method");
+       reverseInteger();
+    }
+
+    public static int reverseInteger(){
+       System.out.println("Reverse Integer Method");
+       return 2;
+    }
+}
