edd3a311d4ed3f62e3782ad989213af80b58a7bd
uonyekwuluje <uionyekwuluje@gmail.com>
2020-11-13 12:15:45 -0500

    adding cheatsheets

---
+++ javaExercises/selection_sort/SelectionSort.java
@@ -0,0 +1,41 @@
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+
+public class SelectionSort{
+   public static void main(String[] args){
+       int[] nums = {6,5,3,11,8,7,2,1,9,4,13,-3,12,0};
+       System.out.println("Unsorted Array => "+Arrays.toString(nums));
+       //smallest1(nums);
+       smallest2(nums);
+   }
+
+
+   public static void smallest1(int[] intarg){
+       int temp = 0;
+       for (int x = 0; x < intarg.length; x++) {
+         for (int y = x+1; y < intarg.length; y++) {
+            if (intarg[x] > intarg[y]) {
+               temp = intarg[x];
+               intarg[x] = intarg[y];
+               intarg[y] = temp;
+            }
+         }
+       }
+       System.out.println("The Smallest value "+intarg[0]);
+   }
+
+
+   public static void smallest2(int[] intarg){
+       int smallest = 0;
+       int largest = 0;
+       for (int x = 0; x < intarg.length; x++) {
+           if (intarg[x] < smallest) {
+             smallest = intarg[x];
+           } else if (intarg[x] > largest){  
+             largest = intarg[x];
+           }      
+       }
+       System.out.println("The Smallest value "+smallest);
+       System.out.println("The Largest value "+largest);
+   }
+}
