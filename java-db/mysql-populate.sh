29daaf11de8755c20479179448b4b200a8fdc285
uonyekwuluje <uionyekwuluje@gmail.com>
2020-12-15 01:56:08 -0500

    updating mysql script

--- java-db/mysql-populate.sh
+++ java-db/mysql-populate.sh
@@ -3,7 +3,7 @@
 mysql_script_file="studentdb.sql"
 mysql_username="$1"
 mysql_password="$2"
-mysql_database="studentdb"
 
-# Connect MySQL
-mysql -u ${mysql_username} -p${mysql_password} ${mysql_database} < ${mysql_script_file}
+#mysql_database="studentdb"
+#mysql -u ${mysql_username} -p${mysql_password} ${mysql_database} < ${mysql_script_file}
+mysql -u ${mysql_username} -p${mysql_password} < ${mysql_script_file}
